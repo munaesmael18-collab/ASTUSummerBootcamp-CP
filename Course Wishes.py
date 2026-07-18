e874cf5bba8c2e1ef0eb9daf6db3e39e0425553a
Muna Esmael <munaesmael18@gmail.com>
2026-07-18 17:02:45 +0530

    Add course wishes processing logic
    Implement logic to process course wishes and track operations.

---
+++ Course Wishes.py
@@ -0,0 +1,15 @@
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    operations = []
+    for level in range(k, 0, -1):
+        for i in range(n):
+            if b[i] == level:
+                while b[i] <= k:
+                    operations.append(i + 1)
+                    b[i] += 1
+    print(len(operations))
+    if operations:
+        print(*operations)
